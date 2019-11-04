6c98b6418137e59faec5b8c3ba72b4e5207f5211
Anucha Maitripirom <zanucha@tisco.co.th>
2019-11-04 15:09:22 +0700

    update .bash_profile using sdk_root as brew installed

--- bash_profile_setting.sh
+++ bash_profile_setting.sh
@@ -1,12 +1,11 @@
-export ANDROID_HOME=/Users/mobiledev/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
+export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
+export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
 
 export JAVA_HOME=$(/usr/libexec/java_home)
 export PATH=$PATH:$JAVA_HOME/bin
 
 export PATH=/usr/local/opt/node@10/bin:$PATH
-export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
 
 appium &
