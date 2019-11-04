82bce844b11a4c72be9b28e87124ef88fa063d3b
Anucha Maitripirom <zanucha@tisco.co.th>
2019-11-04 14:07:54 +0700

    source bash_profile after create bash_profile

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # edit bash_profile
 echo "$(cat bash_profile_setting.sh )" >> $HOME/.bash_profile
 
+source $HOME/.bash_profile
+
 # install home brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
