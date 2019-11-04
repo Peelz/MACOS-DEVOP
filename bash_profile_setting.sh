372cfafbc084f8a41993d062fd0d8caf5f1f47e2
Anucha Maitripirom <zanucha@tisco.co.th>
2019-11-04 14:47:38 +0700

    add ANDROID_SDK_ROOT for bash_profile

--- bash_profile_setting.sh
+++ bash_profile_setting.sh
@@ -7,5 +7,6 @@ export JAVA_HOME=$(/usr/libexec/java_home)
 export PATH=$PATH:$JAVA_HOME/bin
 
 export PATH=/usr/local/opt/node@10/bin:$PATH
+export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
 
 export appium &
