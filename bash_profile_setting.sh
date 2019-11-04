0a0c373f0d09bc3a2a2baf1cf8ee99168f90076c
Anucha Maitripirom <zanucha@tisco.co.th>
2019-11-04 15:40:14 +0700

    fixed default appium port checeking

--- bash_profile_setting.sh
+++ bash_profile_setting.sh
@@ -9,6 +9,6 @@ export PATH=$PATH:$JAVA_HOME/bin
 export PATH=/usr/local/opt/node@10/bin:$PATH
 
 # check appium is running
-if [ ! -n "$(lsof -n -i:1 | grep LISTEN)" ]; then 
+if [ ! -n "$(lsof -n -i:4723 | grep LISTEN)" ]; then 
     appium &
 fi
